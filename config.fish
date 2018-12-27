77a6fee57c7e05577d2b56df521588e49c6acbcd
justinbeharry <jsbeharr@umich.edu>
2018-12-26 23:11:26 -0500

    Add support for colored man pages

--- config.fish
+++ config.fish
@@ -25,6 +25,14 @@ alias vimdiff "nvim -d"
 alias mapscii "telnet mapscii.me"
 alias starii "telnet towel.blinkenlights.nl"
 
+# Colored Man
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
 # andaconda shell integration
 # source $HOME/.config/fish/conda.fish
 
