6eab5fb7fe32c0ac1691b3bea213860a82f391ac
justinbeharry <jsbeharr@umich.edu>
2019-02-08 17:24:41 -0500

    Add conditional statements for OS's
    Change alias and abbr depending on OS

--- config.fish
+++ config.fish
@@ -16,11 +16,12 @@ alias all "ls -R * | less"
 # List all dot files
 alias dfs "ls -A | grep \"^\.\""
 
-# list copy output to clipboard
-alias copy "xclip -selection clipboard"
-
-# Lock the screen on gnome
-alias lock "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"
+if test (uname) = "Linux"
+  # Lock the screen on gnome
+  alias lock "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"
+  # list copy output to clipboard
+  alias copy "xclip -selection clipboard"
+end
 
 # Set vi command to open neovim if neovim is installed
 alias vi "nvim"
@@ -36,9 +37,16 @@ abbr -a -g gco git checkout
 abbr -a -g gaa git add -A
 abbr -a -g gcm git commit
 abbr -a -g gst git status
-abbr -a -g upg sudo pacman -Syyu
-abbr -a -g ins sudo pacman -S
-abbr -a -g rem sudo pacman -Rs
+switch (uname)
+case Linux
+  abbr -a -g upg sudo pacman -Syyu
+  abbr -a -g ins sudo pacman -S
+  abbr -a -g rem sudo pacman -Rs
+case Darwin
+  abbr -a -g upg brew update
+  abbr -a -g ins brew install
+  abbr -a -g rem brew uninstall
+end
 
 # Colored Man
 set -xU LESS_TERMCAP_md (printf "\e[01;31m")
@@ -61,23 +69,25 @@ set -gx GPG_TTY (tty)
 
 # set editor and visual
 if type -q nvim
-    # Set vi command to open neovim if neovim is installed
-    set -gx VISUAL nvim
-    # set aliases for neovim
-    alias vi "nvim"
-    alias vim "nvim"
-    alias vimdiff "nvim -d"
+  # Set vi command to open neovim if neovim is installed
+  set -gx VISUAL nvim
+  # set aliases for neovim
+  alias vi "nvim"
+  alias vim "nvim"
+  alias vimdiff "nvim -d"
 else
-    # Otherwise set the editor to vim
-    set -gx VISUAL vim
+  # Otherwise set the editor to vim
+  set -gx VISUAL vim
 end
 set -gx EDITOR $VISUAL
 
 # Set default browser
-set -gx BROWSER /usr/bin/brave
+if type -q brave
+  set -gx BROWSER /usr/bin/brave
+end
 
 # clear history on terminal exit
 function bye --on-process-exit %self
-    builtin history clear
-    echo Session history scrubbed. Goodbye
+  builtin history clear
+  echo Session history scrubbed. Goodbye
 end
